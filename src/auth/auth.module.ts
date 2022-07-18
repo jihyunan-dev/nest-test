1cf2f9ccbd631174c194fb819d2c6de659fb9bda
jihyun <hwiyu25@gmail.com>
2022-07-19 01:30:09 +0900

    ✨ username, password validation check

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
-import { User } from "./auth.entity";
+import { User } from "./user.entity";
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
-import { User } from "./auth.entity";
+import { User } from "./user.entity";
 import { AuthCredentialDto } from "./dto/auth-credential.dto";
 
 @Injectable()
@@ -12,8 +12,18 @@ export class AuthService {
   ) {}
 
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
-    const { username, password } = authCredentialDto;
-    const user = this.userRepository.create({ username, password });
-    await this.userRepository.save(user);
+    const user = new User();
+    user.username = authCredentialDto.username;
+    user.password = authCredentialDto.password;
+
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === "23505") {
+        throw new ConflictException("Existing username");
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
