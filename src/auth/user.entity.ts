b0b7a4971eb4dd22a92552f2b3e22b5b3d65a3cf
jihyun <hwiyu25@gmail.com>
2022-07-19 21:50:16 +0900

    ✨ 유저와 게시물 관계 형성

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,5 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
+import { Board } from "src/boards/board.entity";
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn, Unique } from "typeorm";
 
 @Entity()
 @Unique(["username"])
@@ -11,4 +12,10 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  // 유저 한 명이 여러개의 게시글을 쓸 수 있음
+  // @OneToMany(Type, inverseSide, Option)
+  // eager: true이면 user 정보를 가져올 때 board도 가져옴
+  @OneToMany((type) => Board, (board) => board.user, { eager: true })
+  boards: Board[];
 }
--- src/boards/board.entity.ts
+++ src/boards/board.entity.ts
@@ -1,4 +1,5 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "src/auth/user.entity";
+import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { BoardStatus } from "./board.model";
 
 @Entity()
@@ -14,4 +15,8 @@ export class Board extends BaseEntity {
 
   @Column()
   status: BoardStatus;
+
+  // 여러 게시글이 한 유저의 글일 수 있음
+  @ManyToOne((type) => User, (user) => user.boards, { eager: false })
+  user: User;
 }
